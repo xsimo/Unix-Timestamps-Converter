0322561a0ef89f9249d8c65a1a62bdd08d6875e4
xsimo <arame@xsimo.ca>
2018-04-05 07:59:22 -0400

    Ajoutez des fichiers projet.

---
+++ Timestamps/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Timestamps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.minuteDebut = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.heureDebut = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteDebut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heureDebut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(17, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1234567890";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(249, 49);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(380, 225);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "minutes";
+            // 
+            // minuteDebut
+            // 
+            this.minuteDebut.AutoSize = true;
+            this.minuteDebut.Location = new System.Drawing.Point(339, 223);
+            this.minuteDebut.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minuteDebut.Name = "minuteDebut";
+            this.minuteDebut.Size = new System.Drawing.Size(35, 20);
+            this.minuteDebut.TabIndex = 23;
+            this.minuteDebut.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(299, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "heure";
+            // 
+            // heureDebut
+            // 
+            this.heureDebut.Location = new System.Drawing.Point(258, 222);
+            this.heureDebut.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.heureDebut.Name = "heureDebut";
+            this.heureDebut.Size = new System.Drawing.Size(35, 20);
+            this.heureDebut.TabIndex = 21;
+            this.heureDebut.Value = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "=>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "<=";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(429, 226);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 17);
+            this.checkBox2.TabIndex = 29;
+            this.checkBox2.Text = "UTC";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 30);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Unix Timestamp Converter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.minuteDebut);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.heureDebut);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.minuteDebut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heureDebut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown minuteDebut;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown heureDebut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Timestamps/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timestamps
+{
+    public partial class Form1 : Form
+    {
+
+        static long UnixTimeStampTicks = 621355968000000000;
+        //DateTime.MaxValue == 3155378975999999999;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            /*DateTime d = new DateTime();
+            long val = d.Ticks - UnixTimeStampTicks;
+            textBox1.Text = (val / 10000000) + "";
+            label1.Text = d.ToLongDateString() + "  " + d.ToLongTimeString();
+            heureDebut.Value = d.Hour;
+            minuteDebut.Value = d.Minute;*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long val = long.Parse(textBox1.Text);
+            val = (val * 10000000);
+            Int64 valTicks = val + UnixTimeStampTicks;
+            Console.WriteLine(valTicks + "");
+            DateTime d = new DateTime(valTicks, DateTimeKind.Utc);
+            DateTime dUTC = new DateTime(d.Ticks);
+            if (!checkBox2.Checked)
+            {
+                TimeZone tz = TimeZone.CurrentTimeZone;
+                d = tz.ToLocalTime(d);
+            }
+            label1.Text = dUTC.ToLongDateString() + "  " + dUTC.ToLongTimeString() + " UTC";
+
+            DateTime dMinuit = new DateTime(d.Year, d.Month, d.Day);
+
+            monthCalendar1.SetDate(dMinuit);
+            monthCalendar1.UpdateBoldedDates();
+            heureDebut.Value = d.Hour;
+            minuteDebut.Value = d.Minute;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime d = monthCalendar1.SelectionStart;
+
+            d = d.AddHours((double)heureDebut.Value);
+            d = d.AddMinutes((double)minuteDebut.Value);
+            if(!checkBox2.Checked)
+            {
+                TimeZone tz = TimeZone.CurrentTimeZone;
+                d = tz.ToUniversalTime(d);
+            }
+            long val = d.Ticks - UnixTimeStampTicks;
+            textBox1.Text= (val/ 10000000 ) + "";
+            label1.Text = d.ToLongDateString() + "  " + d.ToLongTimeString() + " UTC";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            DateTime dUTC = d.ToUniversalTime();
+            long val = dUTC.Ticks - UnixTimeStampTicks;
+            textBox1.Text = (val / 10000000) + "";
+            label1.Text = dUTC.ToLongDateString() + "  " + dUTC.ToLongTimeString() + " UTC";
+            heureDebut.Value = d.Hour;
+            minuteDebut.Value = d.Minute;
+        }
+    }
+}
